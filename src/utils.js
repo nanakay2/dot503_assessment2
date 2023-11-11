799817b40357d496afb583d17f127a8ce26dc415
nanakay2 <nanakwame914@gmail.com>
2023-11-12 10:57:14 +1100

    rename multiplication function

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,7 @@ export function subtractTwoNumbers(firstNumber, secondNumber) {
     return firstNumber - secondNumber
 }
 
-export function multiplyTwoNumbers(firstNumber, secondNumber) {
+export function multiplyNumbers(firstNumber, secondNumber) {
     return firstNumber * secondNumber
 }
 
