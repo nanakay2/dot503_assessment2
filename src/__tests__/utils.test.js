1939a0dbdcb27e80e2894b0e667c173653b46c65
nanakay2 <nanakwame914@gmail.com>
2023-11-12 23:33:13 +1100

    fixed test not running

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,21 +1,21 @@
 import {
-    addTwoNumbers,
+    addNumbers,
     capitalizeText,
     divideTwoNumbers,
-    multiplyTwoNumbers,
+    multiplyNumbers,
     subtractTwoNumbers,
-  } from "./utils";
+  } from "../utils";
   
   describe("utils", () => {
     it("should add two single digit numbers", () => {
-      expect(addTwoNumbers(2, 3)).toBe(5);
+      expect(addNumbers(2, 3)).toBe(5);
     });
     it("should subtract two single digit numbers", () => {
       expect(subtractTwoNumbers(2, 1)).toBe(0);
     });
   
     it("should multiply two single digit numbers", () => {
-      expect(multiplyTwoNumbers(2, 3)).toBe(6);
+      expect(multiplyNumbers(2, 3)).toBe(6);
     });
   
     it("should divide two single digit numbers", () => {
